6650321cbb2238bf5b7f126f68541aab636cbc3e
Daniel Leon <danielleond99@gmail.com>
2022-07-06 22:48:48 -0400

    nestjs/config and modular structure added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/app';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const logger = new Logger();
+  const configServices: ConfigService = app.get(ConfigService)
+  await app.listen(configServices.get("POR"));
+  logger.log(`App running on: ${await app.getUrl()}`)
 }
 bootstrap();
